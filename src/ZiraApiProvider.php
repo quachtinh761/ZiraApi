1cb7ca753be8bb1d613fbdd97f748315dd2c22e2
Nguyen Van Tinh <quachtinh761@gmail.com>
2019-07-12 17:24:31 +0700

    init source code

---
+++ src/ZiraApiProvider.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ * Created by Nguyen Van Tinh
+ * Date: 7/12/19
+ * Time: 5:18 PM
+ */
+
+namespace Zira\Api;
+
+
+use Illuminate\Support\ServiceProvider;
+
+class ZiraApiProvider extends ServiceProvider
+{
+    public function boot()
+    {
+    }
+
+    public function register()
+    {
+    }
+}
